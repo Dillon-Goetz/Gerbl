9baaa1ded6717efdb06de156ace00450805e26d5
Dillon Goetz <dgoetzcodes@gmail.com>
2024-12-29 18:09:26 -0500

    bug fixes and word bank accuracy

--- gerbl.js
+++ gerbl.js
@@ -2,16 +2,16 @@
 const animalList = [
     "Manga", "Addax", "Snake", "Hound", "Civet", "Eagle", "Borer", "Akita", "Husky", 
     "Bully", "Robin", "Sheep", "Cobra", "Gampr", "Snail", "Perch", "Gecko", "Camel", 
-    "Whale", "Shark", "Worms", "Krait", "Beago", "snake", "Tiger", "Fish)", "Takin", 
+    "Whale", "Shark", "Worms", "Krait", "Beago", "snake", "Tiger", "Takin", 
     "Frise", "Squid", "Bilby", "Bison", "Mamba", "Pleco", "Racer", "Boiga", "Bongo", 
     "Booby", "Trout", "Micra", "Hyena", "Viper", "Mouse", "Horse", "Corso", "Geese", 
     "Grebe", "Skate", "Coati", "Adder", "Raven", "Coral", "Crane", "Junco", "Dhole", 
     "Dingo", "Dorgi", "Doxle", "Laika", "Egret", "Goose", "Eider", "Eland", "Shrew", 
     "Tetra", "Feist", "Finch", "Spitz", "Lemur", "Fossa", "Genet", "Guide", "Irish", 
     "Saint", "Goral", "Heron", "Anole", "Guppy", "Horgi", "Human", "Macaw", "Indri", 
-    "Bear)", "Manee", "Kiang", "Leech", "Kishu", "Koala", "Jindo", "Krill", "Scaup", 
-    "Liger", "Llama", "Loach", "Loris", "Stork", "Flea)", "Molly", "Moose", "Potoo", 
-    "Nyala", "Okapi", "Oribi", "Otter", "Lion)", "Corgi", "Prawn", "Quail", "Quoll", "Panda", 
+    "Manee", "Kiang", "Leech", "Kishu", "Koala", "Jindo", "Krill", "Scaup", 
+    "Liger", "Llama", "Loach", "Loris", "Stork", "Molly", "Moose", "Potoo", 
+    "Nyala", "Okapi", "Oribi", "Otter", "Corgi", "Prawn", "Quail", "Quoll", "Panda", 
     "Flies", "Hyrax", "Sable", "Saiga", "Saola", "Roach", "shark", "Skunk", "Sloth", 
     "Cuvac", "viper", "Stoat", "Tapir", "Devil", "Urial", "Veery", "Vireo", "Yabby", 
     "Zebra", "Zokor", "Zorse", ]
@@ -161,129 +161,67 @@ function update() {
     let guess = "";
     document.getElementById("answer").innerText = "";
 
-    //string up the guess word
-    for (let c=0;c<width; c++){
-        let currTile = document.getElementById(row.toString() + "-" + c.toString())
+    // String up the guess word
+    for (let c = 0; c < width; c++) {
+        let currTile = document.getElementById(row.toString() + "-" + c.toString());
         let letter = currTile.innerText;
         guess += letter;
     }
 
     guess = guess.toUpperCase();
-    if (!wordBank.map(word => word.toUpperCase()).includes(guess)){
+    if (!wordBank.map(word => word.toUpperCase()).includes(guess)) {
         document.getElementById("answer").innerText = "Not in animal List";
-        document.getElementById("board").classList.add("shake"); // Add shake class
+        document.getElementById("board").classList.add("shake");
 
-        // Remove the shake class after the animation
         setTimeout(() => {
             document.getElementById("board").classList.remove("shake");
-        }, 500); // Adjust the time (in milliseconds) as needed
-        return; 
+        }, 500);
+        return;
     }
 
-    //start processing game
     let correct = 0;
     let letterCount = {};
 
-    for(let i = 0; i<word.length; i++){
-        const letter = word[i]
-        if (letterCount[letter]){
-            letterCount[letter] += 1;
-        }
-        else{
-            letterCount[letter] = 1;
-        }
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i];
+        letterCount[letter] = (letterCount[letter] || 0) + 1; // More concise way to count letters
     }
 
-    //first iteration, check all the correct ones
-    for (let c = 0; c < width;c++){
+    let promises = [];
+
+    for (let c = 0; c < width; c++) {
         let currTile = document.getElementById(row.toString() + '-' + c.toString());
         let letter = currTile.innerText;
 
-        //is it in correct position? 
-        if (word[c] == letter) {
-            currTile.classList.add("correct");
-            correct += 1;
-            letterCount[letter] -= 1;
-        }
-        //is it in the word? 
-        else if (word.includes(letter) && letterCount[letter] > 0){
-            currTile.classList.add("present")
-            letterCount[letter] -=1
-        }
-        //not in the word
-        else{
-            currTile.classList.add("absent")
-        }
-        
-        if (correct == width){
+        promises.push(new Promise(resolve => {
+            setTimeout(() => {
+                if (word[c] === letter) {
+                    currTile.classList.add("correct");
+                    correct += 1;
+                    letterCount[letter] -= 1;
+                } else if (word.includes(letter) && letterCount[letter] > 0) {
+                    currTile.classList.add("present");
+                    letterCount[letter] -= 1;
+                } else {
+                    currTile.classList.add("absent");
+                }
+                resolve();
+            }, c * 100); // Delay for animation
+        }));
+    }
+
+    Promise.all(promises).then(() => {
+        // This code now runs AFTER all tiles have been animated
+        if (correct === width) {
             gameOver = true;
             document.getElementById("answer").innerText = "Good job!";
+            return; // Important: Exit the function after winning
         }
-    }
 
-    if (!gameOver && row == height - 1) { // Check if it's the last row AND game is not over
-        gameOver = true;
-        document.getElementById("answer").innerText = "The animal was " + word;
-    }
-    //go again, and mark which ones are present but in wrong position
-    for (let c = 0; c < width;c++){
-        let currTile = document.getElementById(row.toString() + '-' + c.toString());
-        let letter = currTile.innerText;
-
-        if(!currTile.classList.contains("correct")){
-            //is it in the word? 
-            if (word.includes(letter)){
-                currTile.classList.add("present")
-            }
-            //not in the word
-            else{
-                currTile.classList.add("absent")
-            }
-        }    
-
-        
-    }
+        if (!gameOver && row === height - 1) {
+            gameOver = true;
+            document.getElementById("answer").innerText = "The animal was " + word;
+            return; // Important: Exit the function after losing
+        }
+    });
 }
-
-
-// function update() {
-//     let correct = 0;
-//     let letterCount = {};
-//     for(let i = 0; i<word.length;i++){
-//         if (letterCount[letter]){
-//             letterCount[letter] += 1;
-//         }else{
-//             letterCount[letter] = 1
-//         } 
-//     }
-
-//     //first iteration, check all the correct ones
-
-//     let promises = []; 
-  
-//     for (let c = 0; c < width; c++) {
-//       let currTile = document.getElementById(row.toString() + '-' + c.toString());
-//       let letter = currTile.innerText;
-  
-//       // Create a promise for each square's update
-//       promises.push(new Promise(resolve => {
-//         setTimeout(() => {
-//           if (word[c] == letter) {
-//             currTile.classList.add("correct");
-//             correct += 1;
-//           } else if (word.includes(letter)) {
-//             currTile.classList.add("present");
-//           } else {
-//             currTile.classList.add("absent");
-//           }
-//           resolve(); // Resolve the promise after the update
-//         }, c * 150); // Delay each square's update slightly
-//       }));
-//     }
-  
-//     Promise.all(promises).then(() => {
-//       if (correct == width) {
-//         gameOver = true;
-//       }
-//     });
-//   }
